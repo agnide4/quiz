20f2062a0aebab650e80068093a085dc82ed6646
Samir Bello <agnide4@gmail.com>
2020-05-20 12:36:23 -0500

    Struggling to make questions show

--- script.js
+++ script.js
@@ -1,6 +1,88 @@
+//$( document ).ready(showQuiz);
+
+
 //Variables declarations
 let sbutton = document.getElementById("start");
 let quiz = document.getElementById("qContainer");
+let ask = document.getElementsByClassName("question");
+let choiceA = document.getElementById("A");
+let choiceB = document.getElementById("B");
+let choiceC = document.getElementById("C");
+let choiceD = document.getElementById("D");
+let answer = document.getElementsByClassName("answer");
+let name = "";
+
+//Questions
+let questions = [
+    {
+        ask: "When a user views a page containing a JavaScript program, which machine actually executes the script?",
+        choiceA: "The User's machine running a Web browser",
+        choiceB: "The Web server",
+        choiceC: "A central machine deep within Netscape's corporate offices",
+        choiceD: "None of the above",
+        isCorrect: "None of the above"
+    },
+    {
+        ask: "What is the correct form to link HTML to JS file?",
+        choiceA: "<script href = “formValidation.js”>",
+        choiceB: "<script source = “formValidation.js”>",
+        choiceC: "<script name = “formValidation.js”>",
+        choiceD: "<script src = “formValidation.js”>",
+        isCorrect: "<script src = “formValidation.js”>"
+    },
+    {
+        ask:"If para1 is the DOM object for a paragraph, what is the correct syntax to change the text within the paragraph?",
+        choiceA: "'New Text?' ",
+        choiceB: " para1.value ='New Text;' ",
+        choiceC:" para1.firstChild.nodeValue= 'New Text'; ",
+        choiceD: "para1.nodeValue='New Text';",
+        isCorrect: "para1.nodeValue='New Text';"
+    },
+    {
+        ask: "JavaScript is interpreted by:",
+        choiceA: "Client",
+        choiceB: "Server",
+        choiceC: "Object",
+        choiceD: "None of the Above",
+        isCorrect: "client"
+    }
+
+]
+
+//Function to show questions in HTML
+let lastQindex = questions.length - 1;
+let currQindex = 0;
+
+
+function showQuiz(){
+    
+    let q = questions[currQindex];
+    ask.innerHTML = "<p>" + q.ask + "</p>";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+    choiceD.innerHTML = q.choiceD;
+    
+}
+
+function checkAnswer(){
+    let score = 0;
+    if (answer === q.isCorrect){
+        document.getElementsByClassName("result").innerHTML = "You are correct";
+        score += 1;
+        document.getElementById("score").innerHTML = "Total point is:" + score;
+    } else {
+        document.getElementsByClassName("result").innerHTML = "Need to study more";
+        //add timer her
+    }
+    
+}
+
+
+
+
+
+
 
 
 
@@ -10,53 +92,17 @@ function getName(){
     return player;        
 }
 
-function showQuiz(){
-    quiz.innerHTML = questions.q1;
 
 
 
-}
 
-let questions = {
-    q1: "When a user views a page containing a JavaScript program, which machine actually executes the script?",
-    q2: "When a user views a page containing a JavaScript program, which machine actually executes the script?",
-    q3: " If para1 is the DOM object for a paragraph, what is the correct syntax to change the text within the paragraph? ",
-    q4: "JavaScript is interpreted by: "
-};
 
-let answers = {
-    choice1: {
-        A: ["The User's machine running a Web browser", 0],
-        B: ["The Web server", 1],
-        C: ["A central machine deep within Netscape's corporate offices", 0 ],
-        D: ["None of the above", 0] 
-    },
-    choice2: {
-        A: ["<script href = “formValidation.js”>", 0],
-        B: ["<script source = “formValidation.js”>", 1],
-        C: ["<script name = “formValidation.js”>", 0],
-        D: ["<script src = “formValidation.js”>", 0]
-    },
-    choice3: {
-        A: [" 'New Text?' ", 0],
-        B: [" para1.value ='New Text;' ", 0],
-        C: [" para1.firstChild.nodeValue= 'New Text'; ", 0],
-        D: ["para1.nodeValue='New Text';"]
-    },
-    choice4: {
-        A: ["Client", 0],
-        B: ["Server", 0],
-        C: ["Object", 0],
-        D: ["None of the Above", 1]
-    }
 
-};
     
 
 
 
 
-
-console.log(questions);
-console.log(answers.choice1.A[0]);
+//console.log(questions);
+//console.log(answers.choice1.A[0]);
 
