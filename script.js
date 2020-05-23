10b5c31642b82bba0e9156ad203247ad3a9d46a0
Samir Bello <agnide4@gmail.com>
2020-05-23 12:25:37 -0500

    still working on it

--- script.js
+++ script.js
@@ -24,30 +24,34 @@ let questions = [
         isCorrect: "None of the above"
     },
     {
-        ask: "What is the correct form to link HTML to JS file?",
-        choiceA: "<script href = “formValidation.js”>",
-        choiceB: "<script source = “formValidation.js”>",
-        choiceC: "<script name = “formValidation.js”>",
-        choiceD: "<script src = “formValidation.js”>",
-        isCorrect: "<script src = “formValidation.js”>"
-    },
-    {
-        ask:"If para1 is the DOM object for a paragraph, what is the correct syntax to change the text within the paragraph?",
-        choiceA: "'New Text?' ",
-        choiceB: " para1.value ='New Text;' ",
-        choiceC: "para1.firstChild.nodeValue= 'New Text'; ",
-        choiceD: "para1.nodeValue='New Text';",
-        isCorrect: "para1.nodeValue='New Text';"
-    },
-    {
         ask: "JavaScript is interpreted by:",
         choiceA: "Client",
         choiceB: "Server",
         choiceC: "Object",
         choiceD: "None of the Above",
         isCorrect: "client"
-    }
+    },
 
+    
+    {
+        ask: "What is the correct form to link HTML to JS file?",
+        choiceA: "<script href = 'formValidation.js'>",
+        choiceB: "<script source = 'formValidation.js'>",
+        choiceC: "<script name = 'formValidation.js'>",
+        choiceD: "<script src = 'formValidation.js'>",
+        isCorrect: "<script src = 'formValidation.js'>"
+    },
+    {
+        ask:"If para1 is the DOM object for a paragraph, what is the correct syntax to change the text within the paragraph?",
+        choiceA: "New Text?",
+        choiceB: "para1.value = New Text;",
+        choiceC: "para1.firstChild.nodeValue= New Text; ",
+        choiceD: "para1.nodeValue=New Text;",
+        isCorrect: "para1.nodeValue=New Text;"
+    },
+    
+
+    
 ]
 
 $(sbutton).on('click', showQuiz);
@@ -56,52 +60,62 @@ $(sbutton).on('click', showQuiz);
 //Function to show questions in HTML
 let lastQindex = questions.length - 1;
 let currQindex = 0;
-
 let ans = "";
-function isclicked(){
-        if (choiceA.button == true){
-        alert("am clicked");
-        ans = q.choiceA;
-        console.log(ans);
-    }
-}
+
+
 
 function showQuiz(){
     
     let q = questions[currQindex];
-    let ans = "";
+    
     ask.innerHTML = "<p>" + q.ask + "</p>";
     choiceA.innerHTML = "<button>" + q.choiceA + "</button>";
+    choiceA.addEventListener('click', function(){
+        ans = q.choiceA;
+        checkAnswer(ans);
+    });
     choiceB.innerHTML = "<button>" + q.choiceB + "</button>";
+    choiceB.addEventListener('click', function(){
+        ans = q.choiceB;
+        checkAnswer(ans);
+    });
     choiceC.innerHTML = "<button>" + q.choiceC + "</button>";
-    choiceD.innerHTML = "<button>" + q.choiceD + "</button>";   
+    choiceC.addEventListener('click', function(){
+        ans = q.choiceC;
+        checkAnswer(ans);
+    });
+    choiceD.innerHTML = "<button>" + q.choiceD + "</button>";  
+    choiceD.addEventListener('click', function(){
+        ans = q.choiceD;
+        checkAnswer(ans);
+    });
+    
     
 }
-choiceA.addEventListener('click', myfunction);
-
-function myfunction(){
-    alert("Am here");
-}
-
-
 
 
 let score = 0;
 function checkAnswer(){
-        if (answer === isCorrect){
-        document.getElementsByClassName("result").innerHTML = "You are correct";
+        console.log(currQindex);
+        console.log(ans);
+        if (ans == questions[currQindex].isCorrect){
+        document.getElementById("result").innerHTML = "<h3>You are correct </h3>";
         score += 1;
-        document.getElementById("score").innerHTML = "Total point is:" + score;
+        document.getElementById("score").innerHTML = "<h6>You current score is:</h4>" + score;
     } else {
-        document.getElementsByClassName("result").innerHTML = "Need to study more";
+        document.getElementById("result").innerHTML = "<h3>Need to study more</h3>";
+        document.getElementById("score").innerHTML = "<h6>You current score is:</h4>" + score;
         //add timer her
     }
     
+    currQindex++;
+    quizFlow();
+
 }
 
 function quizFlow(){
-    if (currQindex<lastQindex){
-        currQindex++;
+    if (currQindex<=lastQindex){
+        
         showQuiz();
     }else {
         document.getElementById("score").innerHTML = "Your total score is" + score;
